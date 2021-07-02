555e840b9fa3e21c7cd485b1b5c5339790076343
Mohammad Rakibul Hasan <rkhasan333@gmail.com>
2021-07-02 22:05:00 +0200

    context hands on with class component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 // import Movie from '../src/hoc/Movie.jsx';
 // import Counter from './hooks/Counter.jsx';
-import './App.css';
-import User from './hooks/user.jsx';
+// import './App.css';
+// import User from './hooks/user.jsx';
 
-function App() {
-  return (
-    // <Movie id = {1} />
-    // <Counter />
-    <User />
-  );
+// function App() {
+//   return (
+//     // <Movie id = {1} />
+//     // <Counter />
+//     <User />
+//   );
+// }
+
+// export default App;
+
+//implement with class component
+import React, { Component } from 'react';
+import MoviePage from './context/MoviePage';
+import UserContext from './context/userContext';
+
+class App extends Component {
+  state = { currentUser :  { name : "Rakib" } }
+  render() {
+    return (
+      <UserContext.Provider value={this.state.currentUser}>
+        <div>
+          <MoviePage />
+        </div>
+      </UserContext.Provider>
+      
+    );
+  }
 }
 
 export default App;
---
+++ src/context/MovieList.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import UserContext from './userContext';
+
+class MovieList extends Component {
+    //static contextType = UserContext; // contextType is a static property of MovieList class. this line is executed while creating the class. not after like below
+    
+    componentDidMount() {
+        console.log("User Context", this.context.name)
+    }
+
+    render() {
+        return (
+            <UserContext.Consumer>
+            {
+                value =>  <div>Movie List {value.name} </div>
+            }   
+            </UserContext.Consumer>
+            
+        );
+    }
+}
+
+MovieList.contextType = UserContext; //contextType is a static property of MovieList class
+                                 //static property is a property that belongs to a class. not to an object
+                                 //this line executed after creating the class
+export default MovieList;
